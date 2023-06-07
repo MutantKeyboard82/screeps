6734d1e3c8dcc18b8f9e218e6baa83a42ec661f2
MutantKeyboard82 <106142930+MutantKeyboard82@users.noreply.github.com>
2023-06-07 10:38:20 +0100

    Harvesters

--- main.js
+++ main.js
@@ -1,15 +1,30 @@
 require('prototype.creep');
 
-Memory.requiredHarvesters = 8;
+Memory.requiredAHarvesters = 1;
+Memory.requiredBHarvesters = 1;
+Memory.sourceA = 'e392d40b19ba2b5ebead1c24';
+Memory.sourceB = '68f33270b899461ab1881603';
 
 module.exports.loop = function () {
     console.log('********** Start tick ' + Game.time + ' **********');
     
-    let harvestersCount = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester').length;
+    let harvestersACount = _.filter(Game.creeps, (creep) => 
+        creep.memory.role == 'harvester' && creep.memory.source == 'A').length;
 
-    if (harvestersCount < Memory.requiredHarvesters) {
-        Game.spawns['Spawn1'].spawnCreep( [WORK, CARRY, MOVE], 'harvester' + Game.time, {memory:{role:'harvester'}} );
-        console.log('Spawning Harvester');
+    let harvestersBCount = _.filter(Game.creeps, (creep) => 
+        creep.memory.role == 'harvester' && creep.memory.source == 'B').length;
+
+    if (harvestersACount < Memory.requiredAHarvesters) {
+        Game.spawns['Spawn1'].spawnCreep( [WORK, WORK, MOVE],
+        'harvester' + Game.time, {memory:{role:'harvester', source:'A'}} );
+        console.log('Spawning A Harvester');
+    }
+    else {
+        if (harvestersBCount < Memory.requiredBHarvesters) {
+            Game.spawns['Spawn1'].spawnCreep( [WORK, WORK, MOVE], 
+            'harvester' + Game.time, {memory:{role:'harvester', source:'B'}} );
+            console.log('Spawning B Harvester');
+        }
     }
 
     for (var name in Game.creeps) {
--- prototype.creep.js
+++ prototype.creep.js
@@ -1,14 +1,12 @@
 Creep.prototype.runHarvester = function() {
-    if (this.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
-        var source = Game.getObjectById('32817ae98699dab055b5a10d');
-
-        if(this.harvest(source) == ERR_NOT_IN_RANGE) {
-            this.moveTo(source);
-        }
+    let source;
+    if (this.memory.source == 'A') {
+        source = Game.getObjectById(Memory.sourceA);
     }
     else {
-        if (this.transfer(Game.spawns['Spawn1'], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-            this.moveTo(Game.spawns['Spawn1']);
-        }
+        source = Game.getObjectById(Memory.sourceB);
+    }
+    if(this.harvest(source) == ERR_NOT_IN_RANGE) {
+        this.moveTo(source);
     }
 }
